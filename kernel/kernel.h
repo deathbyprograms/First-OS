7c82ce18234a7f649cfc239dbb05156b2f710d39
deathbyprograms <deathbyprograms@gmail.com>
2017-04-18 09:23:38 -0500

    Updated header

---
+++ kernel/kernel.h
@@ -0,0 +1,9 @@
+void main();
+void print(char* message);
+void clear();
+void print_at(char* message, int print_row, int print_col);
+void memory_copy(char* source, char* dest, int bytes);
+void handle_scrolling();
+void new_line();
+void change_attribute(int attribute);
+void default_attribute();
